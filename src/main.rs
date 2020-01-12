b6f1a680161c416bf7d94cae5c6a9b6995c436cb
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-01-12 01:30:34 +0000

    sgadg

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate piston_window;
+
+use piston_window::*;
+
+fn main() {
+	let mut window: PistonWindow =
+		WindowSettings::new("Red Rectangle!", [512; 2])
+			.build().unwrap();
+	while let Some(e) = window.next() {
+		window.draw_2d(&e, |c, g, _| {
+			clear([0.5, 0.5, 0.5, 1.0], g);
+			rectangle([1.0, 0.0, 0.0, 1.0], // red
+				[0.0, 0.0, 100.0, 100.0], // rectangle
+				c.transform, g);
+		});
+	}
+}
